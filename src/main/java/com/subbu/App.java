23db83123704e8aba94b6f395667f09d44e44cc7
subbu <119589102+subbumadu@users.noreply.github.com>
2022-12-13 19:21:10 +0530

    Update App.java

--- src/main/java/com/subbu/App.java
+++ src/main/java/com/subbu/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! this is final project of subbu jwenkins" );
+        System.out.println( "Hello World! this is final project of subbu jenkins" );
     }
 }
