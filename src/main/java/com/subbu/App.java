c984a4f084f1b21455ea3cd895bb0190c777a4cc
subbumadu <subbumadu8@gmail.com>
2022-12-13 13:01:22 +0000

    ok jenkinsproject commited

---
+++ src/main/java/com/subbu/App.java
@@ -0,0 +1,13 @@
+package com.subbu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
